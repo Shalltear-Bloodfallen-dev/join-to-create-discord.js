47e1df9b79406bc4188305dab8757060726184ad
ΛLLΛN <63749036+Shalltear-Bloodfallen-dev@users.noreply.github.com>
2021-10-24 21:30:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const client = new Discord.Client({
+    intents: [
+      Discord.Intents.FLAGS.GUILD_VOICE_STATES
+    ]
+});
+
 const config = require("./config.json");
 
 client.on("ready", ()=>console.log("READY"));
